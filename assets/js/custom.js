b210ab3ee0586c48b80d8eaa4225df82cbb492a9
luthfibintang <luthfibintang8@gmail.com>
2024-10-25 00:41:48 +0700

    Modification index.html

---
+++ assets/js/custom.js
@@ -0,0 +1,24 @@
+// Script for glowing cursor
+const body = document.querySelector('body');
+
+// Update glow when mouse moves
+document.addEventListener('mousemove', function (e) {
+  const x = e.clientX;
+  const y = e.clientY;
+
+  body.style.setProperty('--cursor-x', `${x}px`);
+  body.style.setProperty('--cursor-y', `${y}px`);
+
+  // Make sure the glow is visible when the cursor is inside the window
+  body.style.setProperty('--glow-opacity', `1`);
+});
+
+// Hide glow when cursor leaves the browser window
+document.addEventListener('mouseleave', function () {
+  body.style.setProperty('--glow-opacity', `0`);
+});
+
+    // Show glow again when cursor enters the browser window
+document.addEventListener('mouseenter', function () {
+  body.style.setProperty('--glow-opacity', `1`);
+});
